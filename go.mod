1fd0bcdf3aaa9625407c573f02a4858ee6ce4ba3
Vsevolod <vsevolod.sevostyanov@mediaelements.com>
2021-02-09 21:10:55 +0100

    * moved to v2, implementing interface

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module k9UI
 go 1.13
 
 require (
-	fyne.io/fyne v1.3.3
+	fyne.io/fyne/v2 v2.0.0
 	k8s.io/apimachinery v0.17.0
 	k8s.io/client-go v0.17.0
 )
--- main.go
+++ main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"fyne.io/fyne/app"
-	"fyne.io/fyne/widget"
-	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 	"os"
-	"path/filepath"
 )
 
 func homeDir() string {
@@ -24,74 +19,88 @@ func main() {
 	app := app.New()
 
 	w := app.NewWindow("k8UI")
-	item1 := widget.NewTabItem("Contexts",widget.NewLabel("Context 1"))
-	item2 := widget.NewTabItem("Pods",widget.NewLabel("Hello Fyne222!"))
-	item3 := widget.NewTabItem("Containers",widget.NewLabel("Hello Fyne222!"))
-	item4 := widget.NewTabItem("Port forwards",widget.NewLabel("Hello Fyne222!"))
-	item5 := widget.NewTabItem("Services",widget.NewLabel("Hello Fyne222!"))
-	item6 := widget.NewTabItem("Deployments",widget.NewLabel("Hello Fyne222!"))
-	testLabel := widget.NewLabel("Hello Fyne222!");
-	container := widget.NewTabContainer(item1, item2, item3, item4, item5, item6)
-	w.SetContent(widget.NewVBox(
-		testLabel,
-		container,
-		widget.NewButton("Quit", func() {
-			app.Quit()
-		}),
-		widget.NewButton("Add", func() {
-			container.Append(widget.NewTabItem("Contexts12312312",widget.NewLabel("Context 13123123")))
-		}),
-		widget.NewButton("Remove", func() {
-			container.RemoveIndex(5)
-		}),
-		widget.NewButton("Mouse", func() {
-			var kubeconfig *string
-			if home := homeDir(); home != "" {
-				kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-			} else {
-				kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-			}
-			flag.Parse()
+	content := container.NewMax()
+	content.Add(widget.NewButton("Test", func() {}))
 
-			// use the current context in kubeconfig
-			config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-			if err != nil {
-				panic(err.Error())
-			}
+	split := container.NewHSplit(makeNav(), content)
 
-			// create the clientset
-			clientset, err := kubernetes.NewForConfig(config)
-			if err != nil {
-				panic(err.Error())
-			}
-			for {
-				pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
-				if err != nil {
-					panic(err.Error())
-				}
-				fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+	w.SetContent(split)
 
-				// Examples for error handling:
-				// - Use helper functions like e.g. errors.IsNotFound()
-				// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-				namespace := "default"
-				pod := "example-xxxxx"
-				_, err = clientset.CoreV1().Pods(namespace).Get(pod, metav1.GetOptions{})
-				if errors.IsNotFound(err) {
-					fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
-				} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-					fmt.Printf("Error getting pod %s in namespace %s: %v\n",
-						pod, namespace, statusError.ErrStatus.Message)
-				} else if err != nil {
-					panic(err.Error())
-				} else {
-					fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
-				}
+	//item1 := widget.NewTabItem("Contexts",widget.NewLabel("Context 1"))
+	//item2 := widget.NewTabItem("Pods",widget.NewLabel("Hello Fyne222!"))
+	//item3 := widget.NewTabItem("Containers",widget.NewLabel("Hello Fyne222!"))
+	//item4 := widget.NewTabItem("Port forwards",widget.NewLabel("Hello Fyne222!"))
+	//item5 := widget.NewTabItem("Services",widget.NewLabel("Hello Fyne222!"))
+	//item6 := widget.NewTabItem("Deployments",widget.NewLabel("Hello Fyne222!"))
+	//testLabel := widget.NewLabel("Hello Fyne222!");
+	//container := widget.NewTabContainer(item1, item2, item3, item4, item5, item6)
+	//w.SetContent(widget.NewVBox(
+	//	testLabel,
+	//	container,
+	//	widget.NewButton("Quit", func() {
+	//		app.Quit()
+	//	}),
+	//	widget.NewButton("Add", func() {
+	//		container.Append(widget.NewTabItem("Contexts12312312",widget.NewLabel("Context 13123123")))
+	//	}),
+	//	widget.NewButton("Remove", func() {
+	//		container.RemoveIndex(5)
+	//	}),
+	//	widget.NewButton("Mouse", func() {
+	//		var kubeconfig *string
+	//		if home := homeDir(); home != "" {
+	//			kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	//		} else {
+	//			kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	//		}
+	//		flag.Parse()
+	//
+	//		// use the current context in kubeconfig
+	//		config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	//		if err != nil {
+	//			panic(err.Error())
+	//		}
+	//
+	//		// create the clientset
+	//		clientset, err := kubernetes.NewForConfig(config)
+	//		if err != nil {
+	//			panic(err.Error())
+	//		}
+	//		for {
+	//			pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+	//			if err != nil {
+	//				panic(err.Error())
+	//			}
+	//			fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+	//
+	//			// Examples for error handling:
+	//			// - Use helper functions like e.g. errors.IsNotFound()
+	//			// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+	//			namespace := "default"
+	//			pod := "example-xxxxx"
+	//			_, err = clientset.CoreV1().Pods(namespace).Get(pod, metav1.GetOptions{})
+	//			if errors.IsNotFound(err) {
+	//				fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
+	//			} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+	//				fmt.Printf("Error getting pod %s in namespace %s: %v\n",
+	//					pod, namespace, statusError.ErrStatus.Message)
+	//			} else if err != nil {
+	//				panic(err.Error())
+	//			} else {
+	//				fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
+	//			}
+	//
+	//			//time.Sleep(10 * time.Second)
+	//		}
+	//	}),
+	//))
+	//
+	w.Show()
+	app.Run()
+}
 
-				//time.Sleep(10 * time.Second)
-			}
-		}),
-	))
+func makeNav() fyne.CanvasObject {
+	tree := &widget.Tree{}
 
-	w.ShowAndRun()
+	return container.NewBorder(nil, nil, nil, nil, tree)
 }
