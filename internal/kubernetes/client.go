2fffb6b5cc855b9fa3979894f7b97c438c139f37
Vsevolod <vsevolod.sevostyanov@mediaelements.com>
2021-03-01 16:49:32 +0100

    * amendments on fileload

--- internal/kubernetes/client.go
+++ internal/kubernetes/client.go
@@ -1,30 +1,15 @@
 package kubernetes
 
 import (
-	"flag"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
-	"path/filepath"
 )
 
 var client *kubernetes.Clientset
 
-func getHomeConfig() *string {
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
-
-	return kubeconfig
-}
-
 func getConfig(configPath *string) *rest.Config {
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	config, err := clientcmd.BuildConfigFromFlags("", *configPath)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -42,5 +27,5 @@ func getClient(config *rest.Config) *kubernetes.Clientset {
 }
 
 func GetClient() *kubernetes.Clientset {
-	return getClient(getConfig(getHomeConfig()))
+	return getClient(getConfig(GetConfigPath("kubeconfig")))
 }
--- internal/kubernetes/contexts.go
+++ internal/kubernetes/contexts.go
@@ -1,34 +1,24 @@
 package kubernetes
 
 import (
-	"flag"
-
 	"github.com/goccy/go-yaml"
 	"io/ioutil"
 
 	"os"
-	"path/filepath"
-
 	"strings"
 )
 
 var kubeconfig *string
 
-func homeDir() string {
-	if h := os.Getenv("HOME"); h != "" {
-		return h
-	}
-	return os.Getenv("USERPROFILE") // windows
-}
-
 func GetContexts() []string {
 	if kubeconfig == nil {
-		if home := homeDir(); home != "" {
-			kubeconfig = flag.String("contextreader", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-		} else {
-			kubeconfig = flag.String("contextreader", "", "absolute path to the kubeconfig file")
-		}
-		flag.Parse()
+		kubeconfig = GetConfigPath("contextreader")
+		//if home := homeDir(); home != "" {
+		//	kubeconfig = flag.String("contextreader", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+		//} else {
+		//	kubeconfig = flag.String("contextreader", "", "absolute path to the kubeconfig file")
+		//}
+		//flag.Parse()
 	}
 
 	configYaml, err := os.Open(*kubeconfig)
